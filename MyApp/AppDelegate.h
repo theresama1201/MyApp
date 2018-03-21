d454c2e96e749f52d940630eae6e74da02f3f11b
Theresa.Ma <theresama@augmentum.com.cn>
2018-03-21 16:44:40 +0800

    Initial Commit

---
+++ MyApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyApp
+//
+//  Created by Theresa.Ma on 2018/3/21.
+//  Copyright © 2018年 Theresa.Ma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyApp
+//
+//  Created by Theresa.Ma on 2018/3/21.
+//  Copyright © 2018年 Theresa.Ma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
